1f6ca9e929758e18699fbab953fdff732bfb05bc
ym <1204066670@qq.com>
2022-09-21 14:33:49 +0800

    20220921

---
+++ src/LeetCode_HOT100题/寻找两个正序数组的中位数.java
@@ -0,0 +1,154 @@
+package LeetCode_HOT100题;
+
+import org.junit.Test;
+
+/**
+ * @Author KingofTetris
+ * @Date 2022/9/21 11:22
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ *
+ * 算法的时间复杂度应该为 O(log (m+n)) 。
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * 示例 2：
+ *
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ *  
+ * 提示：
+ *
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/median-of-two-sorted-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 寻找两个正序数组的中位数 {
+
+    @Test
+    public void test(){
+        int[] nums1 = {2,3};
+        int[] nums2 = {5,7};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] merge = merge(nums1,nums2);
+        int mid  = (n + m) / 2;
+        if (((n+m) & 1) == 1) return merge[mid];
+        else  return (double) (merge[mid] + merge[mid - 1]) / 2;
+    }
+
+    /**
+     * 两个正序数组合并
+     * 这样的复杂度是 O( n + m )
+     * 还没有达到 O(log(n+m)) 的要求
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] merge(int[] nums1,int[] nums2){
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[] res = new int[n + m];
+
+        int i = 0,j = 0;
+        int index = 0 ;
+
+        //先两两比较 把小的赋值给res 然后对应的指针后移
+        while(i < n && j < m){
+          if (nums1[i] < nums2[j]) res[index++] = nums1[i++];
+          else res[index++] = nums2[j++];
+        }
+        //跳出上面的循环说明至少有一个数组遍历完了。
+        //后面直接赋值给res就行了。
+        if (i < n){
+            for (int k = i; k < n; k++) {
+                res[index++] = nums1[k];
+            }
+        }
+
+        if (j < m){
+            for (int k = j; k < m; k++) {
+                res[index++] = nums2[k];
+            }
+        }
+        return res;
+    }
+
+
+
+    //有序数组，自然会想到用二分查找 可以达到log(n+m)
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int length1 = nums1.length, length2 = nums2.length;
+        int totalLength = length1 + length2;
+        if (totalLength % 2 == 1) {
+            int midIndex = totalLength / 2;
+            double median = getKthElement(nums1, nums2, midIndex + 1);
+            return median;
+        } else {
+            int midIndex1 = totalLength / 2 - 1, midIndex2 = totalLength / 2;
+            double median = (getKthElement(nums1, nums2, midIndex1 + 1) + getKthElement(nums1, nums2, midIndex2 + 1)) / 2.0;
+            return median;
+        }
+    }
+
+    public int getKthElement(int[] nums1, int[] nums2, int k) {
+        /* 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+         * 这里的 "/" 表示整除
+         * nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+         * nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+         * 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+         * 这样 pivot 本身最大也只能是第 k-1 小的元素
+         * 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+         * 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+         * 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+         */
+
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = 0, index2 = 0;
+        int kthElement = 0;
+
+        while (true) {
+            // 边界情况
+            if (index1 == length1) {
+                return nums2[index2 + k - 1];
+            }
+            if (index2 == length2) {
+                return nums1[index1 + k - 1];
+            }
+            if (k == 1) {
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+
+            // 正常情况
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, length1) - 1;
+            int newIndex2 = Math.min(index2 + half, length2) - 1;
+            int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+            if (pivot1 <= pivot2) {
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            } else {
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
+        }
+    }
+
+    /*作者：LeetCode-Solution
+    链接：https://leetcode.cn/problems/median-of-two-sorted-arrays/solution/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-s-114/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+}
---
+++ src/每日一题/相似度为K的字符串_2022_09_21.java
@@ -0,0 +1,113 @@
+package 每日一题;
+
+import javafx.util.Pair;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * @Author KingofTetris
+ * @Date 2022/9/21 10:10
+ * 对于某些非负整数 k ，如果交换 s1 中两个字母的位置恰好 k 次，能够使结果字符串等于 s2 ，则认为字符串 s1 和 s2 的 相似度为 k 。
+ *
+ * 给你两个字母异位词 s1 和 s2 ，返回 s1 和 s2 的相似度 k 的最小值。
+ *
+ * 示例 1：
+ *
+ * 输入：s1 = "ab", s2 = "ba"
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：s1 = "abc", s2 = "bca"
+ * 输出：2
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s1.length <= 20
+ * s2.length == s1.length
+ * s1 和 s2  只包含集合 {'a', 'b', 'c', 'd', 'e', 'f'} 中的小写字母
+ * s2 是 s1 的一个字母异位词
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/k-similar-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 相似度为K的字符串_2022_09_21 {
+
+    @Test
+    public void test(){
+        String s1 = "abccaacceecdeea";
+        String s2 = "bcaacceeccdeaae";
+        int i = kSimilarity(s1, s2);
+        System.out.println(i);
+    }
+
+    //TODO 困难题 看不懂。
+    /**
+     * 最少交换次数是不能单纯去找后面第一个一样的字符交换就行了
+     * 这样不是最少次数。
+     *
+     * * 提示：
+     *  *
+     *  * 1 <= s1.length <= 20
+     *  * s2.length == s1.length
+     *  * s1 和 s2  只包含集合 {'a', 'b', 'c', 'd', 'e', 'f'} 中的小写字母 这条比较关键只包含 6 种不同的字符
+     *  * s2 是 s1 的一个字母异位词
+     *
+     *  我们可以枚举所有可能的交换方案，在搜索时进行减枝从而提高搜索效率，最终找到最小的交换次数。
+     *
+     */
+    public int kSimilarity(String s1, String s2) {
+        int n = s1.length();
+        Queue<Pair<String, Integer>> queue = new ArrayDeque<Pair<String, Integer>>();
+        Set<String> visit = new HashSet<String>();
+        queue.offer(new Pair<String, Integer>(s1, 0));
+        visit.add(s1);
+        int step = 0;
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                Pair<String, Integer> pair = queue.poll();
+                String cur = pair.getKey();
+                int pos = pair.getValue();
+                if (cur.equals(s2)) {
+                    return step;
+                }
+                while (pos < n && cur.charAt(pos) == s2.charAt(pos)) {
+                    pos++;
+                }
+                for (int j = pos + 1; j < n; j++) {
+                    if (s2.charAt(j) == cur.charAt(j)) {
+                        continue;
+                    }
+                    if (s2.charAt(pos) == cur.charAt(j)) {
+                        String next = swap(cur, pos, j);
+                        if (!visit.contains(next)) {
+                            visit.add(next);
+                            queue.offer(new Pair<String, Integer>(next, pos + 1));
+                        }
+                    }
+                }
+            }
+            step++;
+        }
+        return step;
+    }
+
+    public String swap(String cur, int i, int j) {
+        char[] arr = cur.toCharArray();
+        char c = arr[i];
+        arr[i] = arr[j];
+        arr[j] = c;
+        return new String(arr);
+    }
+
+ /*   作者：LeetCode-Solution
+    链接：https://leetcode.cn/problems/k-similar-strings/solution/xiang-si-du-wei-k-de-zi-fu-chuan-by-leet-8z10/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+}
