427f4c60697b80733b3aad6640bada641ecb4ca6
KingOfTetris <1204066670@qq.com>
2022-09-22 14:04:27 +0800

    test

---
+++ src/剑指offer第二版/树/剑指Offer68_II_二叉树的最近公共祖先.java
@@ -0,0 +1,99 @@
+package 剑指offer第二版.树;
+
+import LeetCode数据结构入门.day5.树.TreeNode;
+
+/**
+ * @author by KingOfTetris
+ * @date 2022/9/20
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ *
+ *
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ *  
+ *
+ * 说明:
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉树中。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 剑指Offer68_II_二叉树的最近公共祖先 {
+
+    /**
+     * 和上面的区别是这里是普通的二叉树
+     *
+     * 那么最近公共祖先只有下面三种情况
+     * 若 root 是 p, q的 最近公共祖先 ，则只可能为以下情况之一：
+     * p 和 q 在 root 的子树中，且分列 root 的 异侧（即分别在左、右子树中）；
+     * p = root ，且 q 在 root 的左或右子树中；
+     * q = root ，且 p 在 root 的左或右子树中；
+     *
+     * 作者：jyd
+     * 链接：https://leetcode.cn/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-ii-er-cha-shu-de-zui-jin-gong-gon-7/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //按照先序遍历的顺序 遍历二叉树
+        if (root == null ) return null;//空树返回null
+        if(root == p || root == q) return root;//如果遇到这两个节点就返回给父节点
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q); //左子树
+        TreeNode right = lowestCommonAncestor(root.right, p, q); //右子树
+
+        //两边都不空，那么root就是结果
+        if (right != null && left != null) return root;
+        //剩下一边就返回 不为空的
+        if (left != null) return left;
+        if (right != null) return right;
+
+
+        //left right都为空
+        return null;
+
+      /*  作者：jyd
+        链接：https://leetcode.cn/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-ii-er-cha-shu-de-zui-jin-gong-gon-7/
+        来源：力扣（LeetCode）
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+    }
+
+    /**
+     * 注解版
+     * @param root
+     * @param p
+     * @param q
+     * @return
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null; // 如果树为空，直接返回null
+        if(root == p || root == q) return root; // 如果 p和q中有等于 root的，那么它们的最近公共祖先即为root（一个节点也可以是它自己的祖先）
+        TreeNode left = lowestCommonAncestor(root.left, p, q); // 递归遍历左子树，只要在左子树中找到了p或q，则先找到谁就返回谁
+        TreeNode right = lowestCommonAncestor(root.right, p, q); // 递归遍历右子树，只要在右子树中找到了p或q，则先找到谁就返回谁
+        if(left == null) return right; // 如果在左子树中 p和 q都找不到，则 p和 q一定都在右子树中，右子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+        else if(right == null) return left; // 否则，如果 left不为空，在左子树中有找到节点（p或q），这时候要再判断一下右子树中的情况，如果在右子树中，p和q都找不到，则 p和q一定都在左子树中，左子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+        else return root; //否则，当 left和 right均不为空时，说明 p、q节点分别在 root异侧, 最近公共祖先即为 root
+    }
+     */
+}
---
+++ src/剑指offer第二版/树/剑指Offer68_I_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,85 @@
+package 剑指offer第二版.树;
+
+import LeetCode数据结构入门.day5.树.TreeNode;
+
+/**
+ * @author by KingOfTetris
+ * @date 2022/9/20
+ *
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ *
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * 示例 2:
+ *
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ *  
+ *
+ * 说明:
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 剑指Offer68_I_二叉搜索树的最近公共祖先 {
+
+    /**
+     * 首先这个一棵BST。下面是最简单想到的递归写法
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val < p.val && root.val < q.val) //比两个值都小，那么去右子树找
+            return lowestCommonAncestor(root.right, p, q);
+        if(root.val > p.val && root.val > q.val) //比两个值都大，那么去右子树找
+            return lowestCommonAncestor(root.left, p, q);
+        return root; //如果一大一小，那OK，这个root就是他们的根节点
+
+       /* 作者：jyd
+        链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-i-er-cha-sou-suo-shu-de-zui-jin-g-7/
+        来源：力扣（LeetCode）
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+    }
+
+
+    /**
+     * 迭代写法
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null) {
+            if(root.val < p.val && root.val < q.val) // p,q 都在 root 的右子树中
+                root = root.right; // 遍历至右子节点
+            else if(root.val > p.val && root.val > q.val) // p,q 都在 root 的左子树中
+                root = root.left; // 遍历至左子节点
+            else break; //找到一大一小了。那么这个节点就是他们的最近公共祖先
+        }
+        return root;
+
+      /*  作者：jyd
+        链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-i-er-cha-sou-suo-shu-de-zui-jin-g-7/
+        来源：力扣（LeetCode）
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+    }
+}
